c3a3cefb5b4ae3cf30f0f434cc00cc482e7994e8
Getulio Sánchez <valentin2507@gmail.com>
2019-01-20 18:58:07 -0500

    Issue #3027198: Simplify logic for extra scopes

--- src/InstagramAuthManager.php
+++ src/InstagramAuthManager.php
@@ -57,12 +57,7 @@ class InstagramAuthManager extends OAuth2Manager {
 
     $extra_scopes = $this->getScopes();
     if ($extra_scopes) {
-      if (strpos($extra_scopes, ',')) {
-        $scopes = array_merge($scopes, explode(',', $extra_scopes));
-      }
-      else {
-        $scopes[] = $extra_scopes;
-      }
+      $scopes = array_merge($scopes, explode(',', $extra_scopes));
     }
 
     return $this->client->getAuthorizationUrl([
