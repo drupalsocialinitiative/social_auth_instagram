871ac9c494c0c6d6370216d334319d5e8ab95be0
Getulio Sánchez <valentin2507@gmail.com>
2019-03-18 11:21:52 -0400

    Issue #3041084: Use Social Auth Network Base

--- src/Plugin/Network/InstagramAuth.php
+++ src/Plugin/Network/InstagramAuth.php
@@ -2,16 +2,11 @@
 
 namespace Drupal\social_auth_instagram\Plugin\Network;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
-use Drupal\Core\Site\Settings;
 use Drupal\Core\Url;
-use Drupal\social_api\Plugin\NetworkBase;
 use Drupal\social_api\SocialApiException;
+use Drupal\social_auth\Plugin\Network\NetworkBase;
 use Drupal\social_auth_instagram\Settings\InstagramAuthSettings;
 use League\OAuth2\Client\Provider\Instagram;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines a Network Plugin for Social Auth Instagram.
@@ -33,67 +28,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class InstagramAuth extends NetworkBase implements InstagramAuthInterface {
 
   /**
-   * The logger factory.
-   *
-   * @var \Drupal\Core\Logger\LoggerChannelFactory
-   */
-  protected $loggerFactory;
-
-  /**
-   * The site settings.
-   *
-   * @var \Drupal\Core\Site\Settings
-   */
-  protected $siteSettings;
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('entity_type.manager'),
-      $container->get('config.factory'),
-      $container->get('logger.factory'),
-      $container->get('settings')
-    );
-  }
-
-  /**
-   * InstagramAuth constructor.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param array $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The configuration factory object.
-   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
-   *   The logger factory.
-   * @param \Drupal\Core\Site\Settings $settings
-   *   The site settings.
-   */
-  public function __construct(array $configuration,
-                              $plugin_id,
-                              array $plugin_definition,
-                              EntityTypeManagerInterface $entity_type_manager,
-                              ConfigFactoryInterface $config_factory,
-                              LoggerChannelFactoryInterface $logger_factory,
-                              Settings $settings) {
-
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $config_factory);
-
-    $this->loggerFactory = $logger_factory;
-    $this->siteSettings = $settings;
-  }
-
-  /**
    * Sets the underlying SDK library.
    *
    * @return \League\OAuth2\Client\Provider\Instagram|false
@@ -106,7 +40,7 @@ class InstagramAuth extends NetworkBase implements InstagramAuthInterface {
 
     $class_name = '\League\OAuth2\Client\Provider\Instagram';
     if (!class_exists($class_name)) {
-      throw new SocialApiException(sprintf('The Instagram Library for the league oAuth not found. Class: %s.', $class_name));
+      throw new SocialApiException(sprintf('The Instagram library for PHP League OAuth2 not found. Class: %s.', $class_name));
     }
 
     /* @var \Drupal\social_auth_instagram\Settings\InstagramAuthSettings $settings */
@@ -149,6 +83,7 @@ class InstagramAuth extends NetworkBase implements InstagramAuthInterface {
       $this->loggerFactory
         ->get('social_auth_instagram')
         ->error('Define Client ID and Client Secret on module settings.');
+
       return FALSE;
     }
 
